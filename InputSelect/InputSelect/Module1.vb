98bbd7b13c19081b56a77ba137834044c117dad4
NathanCavill <31356738+NathanCavill@users.noreply.github.com>
2017-09-19 16:05:04 +0100

    Initial Commit

---
+++ InputSelect/InputSelect/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Using case selection
+        Dim choice As String
+        'Ask user for the number
+        Console.WriteLine("1. Item 1")
+        Console.WriteLine("2. Item 2")
+        Console.WriteLine("3. Item 3")
+        Console.WriteLine("Enter your choice: ")
+        choice = Console.ReadLine
+        'Multiple branches depending on selection
+        Select Case choice
+            Case Is = "1"
+                Console.WriteLine("Item 1 chosen")
+            Case Is = "2"
+                Console.WriteLine("Item 2 chosen")
+            Case Is = "3"
+                Console.WriteLine("Item 3 chosen")
+            Case Is > "3"
+                Console.WriteLine("Invalid input")
+
+        End Select
+        Console.ReadLine()
+
+    End Sub
+
+End Module
